00e12d60ccc3f22fef5455aafac3fb82ca4fb5de
ben <ben@email.com>
2026-04-24 22:02:26 +1200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// hamburger menu script
+
+function openMenu() {
+    document.getElementById("burgerMenu").classList.add("show")
+}
+
+function closeMenu() {
+    document.getElementById("burgerMenu").classList.remove("show")
+}
