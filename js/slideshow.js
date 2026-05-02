097b07b9c17029a8d50a8762e58bc20404086616
ben <ben@email.com>
2026-05-02 17:23:47 +1200

    done i think

---
+++ js/slideshow.js
@@ -0,0 +1,38 @@
+// hamburger menu script
+
+function openMenu() {
+    document.getElementById("burgerMenu").classList.add("show")
+}
+
+function closeMenu() {
+    document.getElementById("burgerMenu").classList.remove("show")
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+} 
